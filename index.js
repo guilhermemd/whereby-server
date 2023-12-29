25194fbc026fd93aff49594eab89e60253f89eb7
guilherme <guilherme.md@gmail.com>
2023-12-29 13:57:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8000;
+const fetch = require("cross-fetch");
+
+require("dotenv").config();
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.whereby.com/v1",
+  headers: {
+    Authorization: `Bearer ${process.env.API_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+});
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST",
+  })
+);
+
+app.post("/create-meeting", async (req, res) => {
+  try {
+    const data = {
+      endDate: "2099-02-18T14:23:00.000Z",
+      fields: ["hostRoomUrl"],
+    };
+
+    const response = await fetch("https://api.whereby.dev/v1/meetings", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log("aqui", response);
+
+    if (response.status === 201) {
+      const responseData = await response.json();
+      res.json(responseData);
+    } else {
+      res
+        .status(response.status)
+        .json({ error: "Erro ao criar reunião no Whereby" });
+    }
+  } catch (error) {
+    console.error("Erro ao criar reunião no Whereby", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+
+// Rota para criar uma nova reunião
+// app.post("/create-meeting", async (req, res) => {
+//   try {
+//     const response = await axiosInstance.post("/meetings", {
+//       // Você pode configurar opções de reunião aqui, consulte a documentação do Whereby para mais detalhes
+//       // body: JSON.stringify(data),
+//     });
+//     console.log({ response });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: "Erro ao criar reunião" });
+//   }
+// });
+
+// Rota para obter informações sobre uma reunião específica
+app.get("/get-meeting/:meetingId", async (req, res) => {
+  try {
+    const { meetingId } = req.params;
+    const response = await axiosInstance.get(`/meetings/${meetingId}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao obter informações da reunião" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
